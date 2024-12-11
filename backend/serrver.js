a10acc517c001cb490515ff5806239c3e07c56da
margiecutie <margiecute09@gmail.com>
2024-12-11 16:32:48 +0800

    ACT4

---
+++ backend/serrver.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+const cors = require('cors');
+
+//express    
+ const app = express ()
+
+//middleware
+app.use(express.json())
+app.use(cors());
+
+app.use((req, res, next) =>{
+   console.log(req.path, req.method)
+   next()
+})
+
+ //routes
+ app.use('/api/workouts', workoutRoutes)
+
+
+ //connect to db
+ mongoose.connect(process.env.MONGO_URI)
+ .then(() =>{
+    //listen for request
+   app.listen(process.env.PORT, () =>{
+   console.log(` connectedto db and  listening on port ${process.env.PORT}`)
+   })
+ })
+ .catch((error) =>{
+      console.log(error)
+ })
+
+
+
